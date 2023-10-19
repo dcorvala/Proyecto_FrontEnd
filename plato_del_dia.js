1a640092b9d5c4a81c45798834a2a78e96275606
Dario Corvalan <dcorvala@gmail.com>
2023-10-18 22:16:21 -0300

    borrado:        "menu_del_d\303\255a/Domingo.html" 	borrado:        "menu_del_d\303\255a/Jueves.html" 	borrado:        "menu_del_d\303\255a/Lunes.html" 	borrado:        "menu_del_d\303\255a/Martes.html" 	borrado:        "menu_del_d\303\255a/Miercoles.html" 	borrado:        "menu_del_d\303\255a/Sabado.html" 	borrado:        "menu_del_d\303\255a/Viernes.html" 	nuevo archivo:  plato_del_dia.css 	nuevo archivo:  plato_del_dia.html 	nuevo archivo:  plato_del_dia.js

---
+++ plato_del_dia.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Replace 'YOUR_EDAMAM_APP_ID' and 'YOUR_EDAMAM_APP_KEY' with your actual Edamam API credentials.
+    const appId = 'de6f5afc';
+    const appKey = '31a9b26561fbfdedfd6f5c012f500776';
+
+    const recipeContainer = document.getElementById("recipe-container");
+
+    // Function to fetch a random recipe from Edamam
+    async function fetchRandomRecipe() {
+        try {
+            const response = await fetch(`https://api.edamam.com/search?q=*&app_id=${appId}&app_key=${appKey}`);
+            const data = await response.json();
+
+            // Get a random recipe from the response
+            const randomRecipe = data.hits[Math.floor(Math.random() * data.hits.length)].recipe;
+
+            // Display the recipe details on the page
+            recipeContainer.innerHTML = `
+                <h2>${randomRecipe.label}</h2>
+                <p>Ingredients:</p>
+                <ul>
+                    ${randomRecipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join("")}
+                </ul>
+                <p><a href="${randomRecipe.url}" target="_blank">View Full Recipe</a></p>
+            `;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Call the fetchRandomRecipe function when the page loads
+    fetchRandomRecipe();
+});
