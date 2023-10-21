467d7219a2c5548ed7ef08e72b259c04f506c120
Dario Corvalan <dcorvala@gmail.com>
2023-10-21 16:25:28 -0300

    modificado:     js/plato_del_dia.js 	nuevo archivo:  plato_del_dia/domingo.html 	nuevo archivo:  plato_del_dia/jueves.html 	nuevo archivo:  plato_del_dia/lunes.html 	nuevo archivo:  plato_del_dia/martes.html 	nuevo archivo:  plato_del_dia/miercoles.html 	nuevo archivo:  plato_del_dia/sabado.html 	nuevo archivo:  plato_del_dia/viernes.html

--- js/plato_del_dia.js
+++ js/plato_del_dia.js
@@ -1,23 +1,24 @@
-// Reemplaza 'YOUR_EDAMAM_APP_ID' y 'YOUR_EDAMAM_APP_KEY' con tus credenciales de Edamam.
-const appId = 'fd13d35f';
-const appKey = '16e58865487ba117829d12763c195ceb';
+// Obtener el elemento de fecha actual
+const fechaActual = new Date();
 
-const recipeContainer = document.getElementById("recipe-container");
+// Obtener el día de la semana (0 para domingo, 1 para lunes, etc.)
+const diaDeLaSemana = fechaActual.getDay();
 
-async function fetchRandomRecipe() {
-    try {
-        const response = await fetch(`https://api.edamam.com/search?q=*&app_id=${appId}&app_key=${appKey}`);
-        const data = await response.json();
-        const randomRecipe = data.hits[Math.floor(Math.random() * data.hits.length)].recipe;
+// Array de URLs de recetas para cada día de la semana
+const urlsRecetas = [
+  "../plato_del_dia/lunes.html",
+  "../plato_del_dia/martes.html",
+  "../plato_del_dia/miercoles.html",
+  "../plato_del_diajueves.html",
+  "../plato_del_dia/viernes.html",
+  "../plato_del_dia/sabado.html",
+  "../plato_del_dia/domingo.html",
+];
 
-        document.getElementById('recipe-title').textContent = randomRecipe.label;
-        document.getElementById('recipe-instructions').textContent = randomRecipe.url;
-
-    } catch (error) {
-        console.error(error);
-    }
+// Redirigir al usuario a la página de receta correspondiente
+if (diaDeLaSemana >= 0 && diaDeLaSemana < urlsRecetas.length) {
+  window.location.href = urlsRecetas[diaDeLaSemana];
+} else {
+  // Si el día de la semana no coincide con ninguna URL, redirigir a una página por defecto
+  window.location.href = "pagina_no_disponible.html";
 }
-
-window.addEventListener('load', fetchRandomRecipe);
-
-
